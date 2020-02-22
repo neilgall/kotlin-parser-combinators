704343ef97d1e0233370b80b2541ae452c51810b
Neil Gall <neilgall@mac.com>
2020-02-22 13:55:35 +0000

    Code developed during screencast

--- src/parsers.kt
+++ src/parsers.kt
@@ -1,8 +1,161 @@
 /**
  * Parser Combinators in Kotlin
  *
- * Neil Gall   https://github.com/neilgall
- *
- * Edinburgh Kotlin User Group, 6 November 2019
+ * Neil Gall
+ * https://github.com/neilgall/kotlin-parser-combinators
+ * https://twitter.com/neilgall
  */
 
+sealed class Result<out T> {
+    data class Ok<T>(val value: T, val rest: String) : Result<T>()
+    data class Err(val expected: String, val input: String) : Result<Nothing>()
+
+    fun <U> map(f: (T) -> U): Result<U> = when(this) {
+        is Err -> this
+        is Ok -> Ok(f(value), rest)
+    }
+
+    fun <U> flatMap(f: (T, String) -> Result<U>): Result<U> = when(this) {
+        is Err -> this
+        is Ok -> f(value, rest)
+    }
+
+    fun mapExpected(f: (String) -> String): Result<T> = when (this) {
+        is Ok -> this
+        is Err -> Err(f(expected), input)
+    }
+}
+
+typealias Parser<T> = (String) -> Result<T>
+
+fun dot(input: String): Result<Unit> =
+    if (input.startsWith("."))
+        Result.Ok(Unit, input.substring(1))
+    else
+        Result.Err("a dot", input)
+
+
+fun integer(input: String): Result<Int> {
+    if (input.isEmpty() || !input[0].isDigit()) {
+        return Result.Err("an integer", input)
+    } else {
+        var value = 0
+        for (i in input.indices) {
+            if (input[i].isDigit()) {
+                value = (value * 10) + (input[i] - '0')
+            } else {
+                return Result.Ok(value, input.substring(i))
+            }
+        }
+        return Result.Ok(value, "")
+    }
+}
+
+
+fun string(s: String): Parser<Unit> = { input ->
+    if (input.startsWith(s))
+        Result.Ok(Unit, input.substring(s.length))
+    else
+        Result.Err("'$s'", input)
+}
+
+fun quotedString(input: String): Result<String> {
+    if (input.isEmpty() || input[0] != '"') {
+        return Result.Err("a quoted string", input)
+    }
+
+    var escaped = false
+    var string = ""
+    for (i in 1 until input.length) {
+        val c = input[i]
+        when {
+            c == '"' && !escaped ->
+                return Result.Ok(string, input.substring(i + 1))
+            c == '\\' && !escaped ->
+                escaped = true
+            else -> {
+                if (escaped) string += '\\'
+                escaped = false
+                string += c
+            }
+        }
+    }
+
+    return Result.Err("a terminated quoted string", input)
+}
+
+fun whitespace(input: String): Result<Unit> {
+    for (i in input.indices) {
+        if (!input[i].isWhitespace())
+            return Result.Ok(Unit, input.substring(i))
+    }
+    return Result.Ok(Unit, input)
+}
+
+
+fun <T1, T2> seq(p1: Parser<T1>, p2: Parser<T2>): Parser<Pair<T1, T2>> = { input ->
+    p1(input).flatMap { r1, rest ->
+        p2(rest).map { r2 -> Pair(r1, r2) }
+    }
+}
+
+fun <T> choice(p1: Parser<T>, p2: Parser<T>): Parser<T> = { input ->
+    when (val r1 = p1(input)) {
+        is Result.Ok -> r1
+        is Result.Err -> p2(input).mapExpected { e -> "${r1.expected} or $e" }
+    }
+}
+
+fun <T, U> Parser<T>.map(f: (T) -> U): Parser<U> = { input ->
+    this(input).map(f)
+}
+
+fun <T, U> Parser<T>.means(u: U): Parser<U> = map { u }
+
+infix fun <T1, T2> Parser<T1>.then(p2: Parser<T2>): Parser<Pair<T1, T2>> =
+    seq(this, p2)
+
+infix fun <T> Parser<T>.or(p2: Parser<T>): Parser<T> =
+    choice(this, p2)
+
+
+fun <X, T> Parser<X>.before(p: Parser<T>): Parser<T> =
+    seq(this, p).map { it.second }
+
+fun <T, Y> Parser<T>.followedBy(y: Parser<Y>): Parser<T> =
+    seq(this, y).map { it.first }
+
+fun <X, T, Y> Parser<T>.between(x: Parser<X>, y: Parser<Y>): Parser<T> =
+    x.before(this).followedBy(y)
+
+fun <T> List<T>.prepend(t: T): List<T> = listOf(t) + this
+
+fun <T> Parser<T>.many(): Parser<List<T>> = { input ->
+    when (val r = this(input)) {
+        is Result.Err -> Result.Ok(emptyList(), input)
+        is Result.Ok -> this.many()(r.rest).map { it.prepend(r.value) }
+    }
+}
+
+fun <T, X> Parser<T>.sepBy(sep: Parser<X>): Parser<List<T>> = { input ->
+    fun tailParse(tail: String): Result<List<T>> =
+        when (val s = sep(tail)) {
+            is Result.Err -> Result.Ok(emptyList(), tail)
+            is Result.Ok -> when (val r = this(s.rest)) {
+                is Result.Err -> r
+                is Result.Ok -> tailParse(r.rest).map { it.prepend(r.value) }
+            }
+        }
+
+    when (val r = this(input)) {
+        is Result.Err -> Result.Ok(emptyList(), input)
+        is Result.Ok -> tailParse(r.rest).map { it.prepend(r.value) }
+    }
+}
+
+
+class ParserRef<T>: Parser<T> {
+    lateinit var p: Parser<T>
+    fun set(p: Parser<T>) { this.p = p }
+    override fun invoke(input: String): Result<T> = p(input)
+}
--- src/parsersTests.kt
+++ src/parsersTests.kt
@@ -1,5 +1,145 @@
+import io.kotlintest.matchers.shouldBe
 import io.kotlintest.specs.StringSpec
 
-class ParsersTests : StringSpec({
+sealed class JSON {
+    object Null : JSON()
+    data class Bool(val value: Boolean) : JSON()
+    data class Number(val value: Int) : JSON()
+    data class String(val value: kotlin.String) : JSON()
+    data class Array(val value: List<JSON>) : JSON()
+    data class Object(val value: Map<kotlin.String, JSON>) : JSON()
+}
 
+class ParserTests : StringSpec({
+
+    "dot" {
+        dot(".foo") shouldBe Result.Ok(Unit, "foo")
+        dot("foo") shouldBe Result.Err("a dot", "foo")
+    }
+
+    "integer" {
+        integer("123foo") shouldBe Result.Ok(123, "foo")
+        integer("foo") shouldBe Result.Err("an integer", "foo")
+    }
+
+    "string" {
+        val p = string("foo")
+        p("foobar") shouldBe Result.Ok(Unit, "bar")
+        p("boofar") shouldBe Result.Err("'foo'", "boofar")
+    }
+
+    "seq" {
+        val p = seq(::integer, string("foo"))
+        p("123foo") shouldBe Result.Ok(Pair(123, Unit), "")
+    }
+
+    "then" {
+        val p = ::integer then string("foo")
+        p("123foo") shouldBe Result.Ok(Pair(123, Unit), "")
+    }
+
+    "choice" {
+        val p = choice(string("foo").means(1), string("bar").means(2))
+        p("foobar") shouldBe Result.Ok(1, "bar")
+        p("barfoo") shouldBe Result.Ok(2, "foo")
+        p("xyz") shouldBe Result.Err("'foo' or 'bar'", "xyz")
+    }
+
+    "or" {
+        val p = string("foo").means(1) or string("bar").means(2)
+        p("foobar") shouldBe Result.Ok(1, "bar")
+        p("barfoo") shouldBe Result.Ok(2, "foo")
+        p("xyz") shouldBe Result.Err("'foo' or 'bar'", "xyz")
+    }
+
+    "before" {
+        val p = string("*").before(::integer)
+        p("*123") shouldBe Result.Ok(123, "")
+    }
+
+    "followedBy" {
+        val p = ::integer.followedBy(string("%"))
+        p("123%") shouldBe Result.Ok(123, "")
+    }
+
+    "between" {
+        val p = ::integer.between(string("<"), string(">"))
+        p("<123>") shouldBe Result.Ok(123, "")
+    }
+
+    "many" {
+        val p = ::dot.means(1).many().map { it.sum() }
+        p("...foo") shouldBe Result.Ok(3, "foo")
+        p("foo") shouldBe Result.Ok(0, "foo")
+    }
+
+    "sepBy" {
+        val p = ::integer.sepBy(string(","))
+        p("1,2,3") shouldBe Result.Ok(listOf(1, 2, 3), "")
+        p("1,2,foo") shouldBe Result.Err("an integer", "foo")
+        p("foo") shouldBe Result.Ok(emptyList<Int>(), "foo")
+    }
+
+    "json list of ints" {
+        val p = ::integer.sepBy(string(",")).between(string("["), string("]"))
+        p("[1,2,3,4]") shouldBe Result.Ok(listOf(1, 2, 3, 4), "")
+    }
+
+    "json" {
+        val jsonRef = ParserRef<JSON>()
+
+        val jsonNull = string("null").means(JSON.Null)
+        val jsonTrue = string("true").means(JSON.Bool(true))
+        val jsonFalse = string("false").means(JSON.Bool(false))
+        val jsonNum = ::integer.map(JSON::Number)
+        val jsonStr = ::quotedString.map(JSON::String)
+
+        fun token(s: String): Parser<Unit> =
+            string(s).between(::whitespace, ::whitespace)
+
+        val jsonArray = jsonRef
+            .sepBy(token(","))
+            .between(token("["), token("]"))
+            .map(JSON::Array)
+
+        val jsonPair = ::quotedString.followedBy(token(":")) then jsonRef
+
+        val jsonObject = jsonPair
+            .sepBy(token(","))
+            .between(token("{"), token("}"))
+            .map { pairs -> JSON.Object(pairs.toMap()) }
+
+        val json = (jsonNull or jsonTrue or jsonFalse or jsonNum or
+                jsonStr or jsonArray or jsonObject)
+
+        jsonRef.set(json)
+
+        json(
+            """
+            { 
+                "foo": "bar",
+                "number": 123,
+                "array": [ 1,2,3 ],
+                "objs": [{"a": true}, {"b": false}],
+                "nothing": null
+            }"""
+        ) shouldBe Result.Ok(
+            JSON.Object(
+                mapOf(
+                    "foo" to JSON.String("bar"),
+                    "number" to JSON.Number(123),
+                    "array" to JSON.Array(listOf(JSON.Number(1), JSON.Number(2), JSON.Number(3))),
+                    "objs" to JSON.Array(
+                        listOf(
+                            JSON.Object(mapOf("a" to JSON.Bool(true))),
+                            JSON.Object(mapOf("b" to JSON.Bool(false)))
+                        )
+                    ),
+                    "nothing" to JSON.Null
+                )
+            ),
+            ""
+        )
+
+    }
 })
